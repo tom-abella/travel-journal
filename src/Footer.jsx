0d002059c32bb01ecbe896529b7426f8d96fb4cd
Tom Leonard Abella <tomleonardabella@gmail.com>
2022-10-11 16:36:56 +0800

    Finalize the App

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Footer(){
+    return(
+        <div className="footer pt-2">
+        <div className="container-fluid">
+            <p><a href="https://tomleonardabella-businesscard.netlify.app/" target="_blank"> Tom Leonard Abella</a> • &copy; 2022  • Travel Journal</p> 
+        </div>
+    </div>
+    )
+}
