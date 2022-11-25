729b452066cdf3c327cf5b6c57f379980e71a8af
Tom Leonard Abella <tomleonardabella@gmail.com>
2022-11-25 10:56:16 +0800

    Redirect the Footer Link

--- .history/src/Footer_20221125105544.jsx
+++ .history/src/Footer_20221125105544.jsx
@@ -4,7 +4,7 @@ export default function Footer(){
     return(
         <div className="footer pt-2">
         <div className="container-fluid">
-            <p><a href="https://tom-abella.github.io/Portfolio/" target="_blank"> Tom Leonard Abella</a> • &copy; 2022  • Travel Journal</p> 
+            <p><a href="https://tom-abella.github.io/Portfolio/#projects" target="_blank"> Tom Leonard Abella</a> • &copy; 2022  • Travel Journal</p> 
         </div>
     </div>
     )
